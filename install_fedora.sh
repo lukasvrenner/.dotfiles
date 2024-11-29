69e3b1c01d37623d81580acd6dc0091d7fca1f62
lukasvrenner <lukasvrenner@icloud.com>
2024-11-29 13:52:23 -0900

    Only store necessary files in .dotfiles

--- install_fedora.sh
+++ install_fedora.sh
@@ -2,9 +2,8 @@
 
 install=()
 
-# graphical
 install+='sway '
-install+='swaybar ' # status bar for sway
+install+='swaybar '
 install+='i3status '
 install+='wmenu ' 
 install+='foot '
@@ -14,9 +13,8 @@ install+='stow' # dotfiles manager
 install+='gcc '
 install+='meson '
 install+='nnn ' # terminal file manager
-install+='gh ' # github CLI
+install+='gh ' # GitHub CLI
 install+='ripgrep '
-
 
 # rust compiler
 echo "installing Rust"
@@ -32,7 +30,6 @@ git clone --depth=1 https://github.com/savq/paq-nvim.git \
     "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim
 
 nvim +PaqInstall +q # install plugins and quit
-
 
 sudo dnf install ${install[*]}
 stow $(dirname $0)
