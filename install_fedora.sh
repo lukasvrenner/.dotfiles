87b3ce2cef167372d2749effd3f8b9ada11bb785
lukasvrenner <lukasvrenner@icloud.com>
2025-04-20 10:46:22 -0800

    install: install vim instead of neovim

--- install_fedora.sh
+++ install_fedora.sh
@@ -12,23 +12,10 @@ install+='Thunar ' # GTK file manager
 install+='stow' # dotfiles manager
 install+='gcc '
 install+='meson '
-install+='nnn ' # terminal file manager
 install+='gh ' # GitHub CLI
-install+='ripgrep '
-
-# rust compiler
-echo "installing Rust"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-cargo component add rust-analyzer # LSP
-
-# C compiler
 install+='gcc '
-
-install+='neovim'
-git clone --depth=1 https://github.com/savq/paq-nvim.git \
-    "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim
-
-nvim +PaqInstall +q # install plugins and quit
+install+='vim '
+install+='rustup '
 
 sudo dnf install ${install[*]}
 stow $(dirname $0)
