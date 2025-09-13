ebbdfa1a350bc01c60870d7df0ab9252089ce2fa
Lukas Renner <lukasvrenner@icloud.com>
2025-09-13 10:27:56 -0800

    install: don't install swaybar because package doesn't exist
    install: make list formatting more idiomatic

--- install_deb.sh
+++ install_deb.sh
@@ -1,32 +1,32 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 
-install=()
+packages=(
+    sway
+    i3status
+    wmenu
+    foot
+    chromium
+    stow
+    gcc
+    universal-ctags
+    meson
+    gh
+    vim
+    rustup
+    imv
+    locate
+    desktop-file-utils # creates a cache to make xdg-open fast
+    pipewire
+    brightnessctl
+    slurp
+    grim
+    fonts-noto-color-emoji
+)
 
-install+='sway '
-install+='swaybar '
-install+='i3status '
-install+='wmenu ' 
-install+='foot '
-install+='chromium '
-install+='thunar ' # GTK file manager
-install+='stow' # dotfiles manager
-install+='gcc '
-install+='ctags '
-install+='meson '
-install+='gh ' # GitHub CLI
-install+='vim '
-install+='rustup '
-install+='imv '
-install+='locate '
-install+='desktop-file-utils ' # creates a cache to make xdg-open fast
-install+='pipewire '
-install+='brightnessctl '
-install+='slurp '
-install+='grim '
-install+='fonts-noto-color-emoji '
-
-sudo apt install  ${install[*]}
+sudo apt install "${packages[@]}"
 
 # don't install recommends to avoid installing yt-dlp and other deps
 sudo apt install --no-install-recommends mpv
+
+mkdir $HOME/.config
 stow $(dirname $0)
